4154789bfb0a302d6b38e1e8dcbf79e3a979955e
Bella Natasya <bellanatasyaam@gmail.com>
2025-11-27 16:22:57 +0700

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -1,10 +1,48 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Customer;
 
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function index()
+    {
+        $customers = Customer::all();
+        return view('customers.index', compact('customers'));
+    }
+
+    public function create()
+    {
+        return view('customers.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:100',
+            'last_name'  => 'required|string|max:100',
+            'email'      => 'required|email|unique:customers,email',
+            'phone'      => 'required|string|max:20',
+            'customer_type' => 'required|in:Regular,VIP',
+        ]);
+
+        // Auto generate ID kalau perlu
+        $customerId = 'CUST-' . strtoupper(substr(uniqid(), -6));
+
+        Customer::create([
+            'customer_id' => $customerId,
+            'first_name' => $request->first_name,
+            'last_name'  => $request->last_name,
+            'email'      => $request->email,
+            'phone'      => $request->phone,
+            'customer_type' => $request->customer_type,
+        ]);
+
+        return redirect()->route('customers.index')
+                        ->with('success', 'Customer created successfully!');
+    }
+
+
 }
