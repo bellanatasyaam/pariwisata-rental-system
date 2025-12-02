4b942d00d570773ec1b77320a2d10ce399535e76
Bella Natasya <bellanatasyaam@gmail.com>
2025-12-02 17:23:04 +0700

    Create JournalEntry.php

---
+++ app/Models/JournalEntry.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JournalEntry extends Model
+{
+    protected $fillable = [
+        'date',
+        'description',
+    ];
+
+    public function details()
+    {
+        return $this->hasMany(JournalEntryDetail::class);
+    }
+}
